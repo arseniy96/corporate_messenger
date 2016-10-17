aa18b14e1998664b181aa2c57a88587a869ee354
arseniy96 <arsenzhar@gmail.com>
2016-10-17 18:19:43 +0300

    Add username column to users_table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161016180605) do
+ActiveRecord::Schema.define(version: 20161017145919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20161016180605) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "username"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
