dca9f57615f00814bb029be186fdca14d664f5d1
arseniy96 <arsenzhar@gmail.com>
2016-10-20 22:23:16 +0300

    Add chats_controller

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,31 @@
+class ChatsController < ApplicationController
+
+  def index
+    @chats = Chat.all
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
