a8ae08251e28efa7071204323c68084bc6938598
arseniy96 <arsenzhar@gmail.com>
2016-10-20 23:46:23 +0300

    Add actions #new and #show for Chat

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,27 +5,49 @@ class ChatsController < ApplicationController
   end
 
   def show
-
+    @chat = Chat.find(params[:id])
+    @user_id = UserChat.where(chat_id: @chat.id).first.user_id
+    @user = User.find(@user_id)
   end
 
   def new
-
+    @chat = Chat.new
   end
 
   def edit
-
+    @chat = Chat.find(params[:id])
   end
 
   def create
-
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      @chat.users << current_user
+      redirect_to @chat
+    else
+      render action: 'new'
+    end
   end
 
   def update
-
+    @chat = Chat.find(params[:id])
+    if @chat.update(chat_params)
+      redirect_to @chat
+    else
+      render action: 'edit'
+    end
   end
 
   def destroy
+    @chat = Chat.find(params[:id])
+    @chat.destroy
 
+    redirect_to chats_path
+  end
+
+  private
+
+  def chat_params
+    params.require(:chat).permit(:title, :description)
   end
 
 end
