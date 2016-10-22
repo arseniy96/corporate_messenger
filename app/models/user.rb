92b33f48d216c2fbe4c34088acb451ab2fb1bc24
arseniy96 <arsenzhar@gmail.com>
2016-10-22 10:28:28 +0300

    Add model Messange and associations has_many

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ActiveRecord::Base
 
   has_many :user_chats
   has_many :chats, through: :user_chats
+  has_many :messanges
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021195945) do
+ActiveRecord::Schema.define(version: 20161022071755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20161021195945) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "user_creator_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "chat_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "user_chats", force: :cascade do |t|
