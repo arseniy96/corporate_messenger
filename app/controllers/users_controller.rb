6959e505c2d9af8f428c54f2078473db23d96492
arseniy96 <arsenzhar@gmail.com>
2016-10-23 22:16:03 +0300

    Add opportunity of seeing potential friends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @friends = @user.users
+    @potential_friends = []
+    people = User.all
+    people.each do |person|
+      if person.users.include?(current_user)
+        @potential_friends << person
+      end
+    end
   end
 
   def add_friend
