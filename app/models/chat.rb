56ea1843923d23ee9a904cc890c6a88d04440197
arseniy96 <arsenzhar@gmail.com>
2016-11-05 22:44:19 +0300

    Room has one Chat

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,5 +1,4 @@
 class Chat < ActiveRecord::Base
-  has_many :user_chats
-  has_many :users, through: :user_chats
+  belongs_to :room
   has_many :messages
 end
--- db/migrate/20161016171552_create_chats.rb
+++ db/migrate/20161016171552_create_chats.rb
@@ -1,8 +1,7 @@
 class CreateChats < ActiveRecord::Migration
   def change
     create_table :chats do |t|
-      t.string :title
-      t.text :description
+      t.references :room
 
       t.timestamps null: false
     end
