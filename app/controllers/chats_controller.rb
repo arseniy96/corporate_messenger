dfb3f4e68b86712c72b1194e97383770ebb00b3a
arseniy96 <arsenzhar@gmail.com>
2016-11-08 18:53:36 +0300

    Minor Changes

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,6 +3,7 @@ class ChatsController < ApplicationController
   def show
     @room = Room.find(params[:id])
     @chat = @room.chat
+    @friends = current_user.users
     render 'rooms/chat'
   end
 
