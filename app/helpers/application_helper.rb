826b69e2f369ce947fe980a264231b2478ad2ed5
arseniy96 <arsenzhar@gmail.com>
2016-12-03 13:45:09 +0300

    Add date_format helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def date_format(str)
+    str = str.split(' ')
+    #str.delete_at(2)
+    date = str[0].split('-')
+    date = date[2].to_s + '.' + date[1].to_s + '.' + date[0].to_s
+    time = str[1].split(':')
+    time = time[0].to_s + ':' + time[1].to_s
+    str = time + ' ' + date
+  end
+
 end
