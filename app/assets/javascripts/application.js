39580e28bcba551771f373ab9b732c67cec44396
arseniy96 <arsenzhar@gmail.com>
2017-03-25 18:17:03 +0300

    Add place for avatar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,13 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap-sprockets
+
+function avatar() {
+  var div1 = $('#header1');
+  var h1 = $(div1).height();
+  var w1 = $(div1).width();
+  div1 = $('#avatar');
+  var h2 = $(div1).height() / 2;
+  var w2 = $(div1).width() / 2;
+  $(div1).css('top', (h1-h2+20-3) + 'px').css('left', (w1-w2+30-3) + 'px'); //3px = border
+}
